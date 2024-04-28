9d7b92fa435f2819b2025a13e1d9fbeddae24b95
Codeaid <codeaid@users.noreply.github.com>
2024-04-28 18:50:23 +0100

    Added Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,31 @@
+import { Container, Separator } from 'components';
+import styles from './Footer.module.css';
+
+export const Footer = () => (
+  <div className={styles.Footer}>
+    <Separator highlight="center" />
+
+    <Container>
+      <div className={styles.FooterContent}>
+        <div className={styles.FooterDisclaimer}>
+          <p>
+            This website is not affiliated, maintained, authorized, endorsed by, or in any way
+            officially connected with Avalanche Studios Group, or any of its subsidiaries or its
+            affiliates. The official Call of the Wild: The Angler website can be found at{' '}
+            <a className="link" href="https://cotwtheangler.com" target="_blank">
+              https://cotwtheangler.com
+            </a>
+            .
+          </p>
+          <p>
+            Call of the Wild: The Angler is a registered trademarks of{' '}
+            <a className="link" href="https://avalanchestudios.com" target="_blank">
+              Avalanche Studios Group
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </Container>
+  </div>
+);
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,7 @@ export * from './FishInfoHeader';
 export * from './FishInfoHooks';
 export * from './FishInfoSection';
 export * from './FishInfoWeight';
+export * from './Footer';
 export * from './Separator';
 export * from './TraitIcon';
 export * from './TraitIconList';
