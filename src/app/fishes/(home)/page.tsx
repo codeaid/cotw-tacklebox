99f5821c0668124a315e336252981066bb19b495
Codeaid <codeaid@users.noreply.github.com>
2024-04-28 21:22:44 +0100

    Removed URL updates when searching on listing page

--- src/app/fishes/(home)/page.tsx
+++ src/app/fishes/(home)/page.tsx
@@ -1,17 +1,13 @@
 'use client';
 
-import { useRouter, useSearchParams } from 'next/navigation';
-import { type ChangeEvent, useCallback, useEffect, useState } from 'react';
+import { type ChangeEvent, useCallback, useState } from 'react';
 import { Input, ReserveFishGrid } from 'components';
 import { reserves } from 'config/reserves';
 import styles from './page.module.css';
 
 const FishesPage = () => {
-  const params = useSearchParams();
-  const router = useRouter();
-
   // Initialise the search query to the current value of the "q" query parameter
-  const [query, setQuery] = useState(() => params.get('q') ?? undefined);
+  const [query, setQuery] = useState('');
 
   /**
    * Update internally stored query value on each input change
@@ -20,15 +16,6 @@ const FishesPage = () => {
     (event: ChangeEvent<HTMLInputElement>) => setQuery(event.target.value.trim()),
     [],
   );
-
-  // Update URL with the new value whenever the search query changes
-  useEffect(() => {
-    if (!query) {
-      router.replace('/fishes');
-    } else {
-      router.replace(`/fishes?q=${query}`);
-    }
-  }, [query, router]);
 
   return (
     <>
