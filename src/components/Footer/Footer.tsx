c61ceadfbaf819f5826326ed3600ecb0579e3660
Codeaid <codeaid@users.noreply.github.com>
2024-04-30 00:50:28 +0100

    Corrected footer disclaimer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,14 +11,14 @@ export const Footer = () => (
           <p>
             This website is not affiliated, maintained, authorized, endorsed by, or in any way
             officially connected with Avalanche Studios Group, or any of its subsidiaries or its
-            affiliates. The official Call of the Wild: The Angler website can be found at{' '}
+            affiliates. The official Call of the Wild: The Angler&#8482; website can be found at{' '}
             <a className="link" href="https://cotwtheangler.com" target="_blank">
               https://cotwtheangler.com
             </a>
             .
           </p>
           <p>
-            Call of the Wild: The Angler is a registered trademarks of{' '}
+            Call of the Wild: The Angler&#8482; is a registered trademark of{' '}
             <a className="link" href="https://avalanchestudios.com" target="_blank">
               Avalanche Studios Group
             </a>
