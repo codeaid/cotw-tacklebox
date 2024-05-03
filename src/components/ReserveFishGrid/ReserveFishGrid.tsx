6b5de46ff79be1c977c825b82fecf99c438941cc
Codeaid <codeaid@users.noreply.github.com>
2024-05-03 21:07:16 +0100

    Included fish count in reserve grid titles

--- src/components/ReserveFishGrid/ReserveFishGrid.tsx
+++ src/components/ReserveFishGrid/ReserveFishGrid.tsx
@@ -1,5 +1,4 @@
 import { FishGrid, Separator } from 'components';
-import { fishes } from 'config/fishes';
 import type { ReserveFishGridProps } from './types';
 import styles from './ReserveFishGrid.module.css';
 
@@ -17,7 +16,10 @@ export const ReserveFishGrid = (props: ReserveFishGridProps) => {
   return (
     <div className={styles.ReserveFishGrid}>
       <div className={styles.ReserveFishGridHeader}>
-        <h2 className={styles.ReserveFishGridTitle}>{reserve.name}</h2>
+        <h2 className={styles.ReserveFishGridTitle}>
+          {reserve.name}
+          <span className={styles.ReserveFishGridTitleCount}>({reserveFishes.length})</span>
+        </h2>
         <Separator highlight="center" />
       </div>
 
