be07c523e52a89086111e10bde66c6b30ddb9520
Codeaid <codeaid@users.noreply.github.com>
2024-05-04 19:18:48 +0100

    Moved footer link styles to its CSS module

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,14 +12,14 @@ export const Footer = () => (
             This website is not affiliated, maintained, authorized, endorsed by, or in any way
             officially connected with Avalanche Studios Group, or any of its subsidiaries or its
             affiliates. The official Call of the Wild: The Angler&#8482; website can be found at{' '}
-            <a className="link" href="https://cotwtheangler.com" target="_blank">
+            <a className={styles.FooterLink} href="https://cotwtheangler.com" target="_blank">
               https://cotwtheangler.com
             </a>
             .
           </p>
           <p>
             Call of the Wild: The Angler&#8482; is a registered trademark of{' '}
-            <a className="link" href="https://avalanchestudios.com" target="_blank">
+            <a className={styles.FooterLink} href="https://avalanchestudios.com" target="_blank">
               Avalanche Studios Group
             </a>
             .
