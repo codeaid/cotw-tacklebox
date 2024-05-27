1ae4990c56b7e1d94507465476bbeaf8f4cc80c7
Codeaid <codeaid@users.noreply.github.com>
2024-05-27 20:15:22 +0100

    Collapsed fish bait rating value ranges

--- src/components/FishInfoBaits/FishInfoBaitsRating.tsx
+++ src/components/FishInfoBaits/FishInfoBaitsRating.tsx
@@ -1,37 +1,18 @@
 import clsx from 'clsx';
-import { useMemo } from 'react';
 import type { FishInfoBaitsRatingProps } from './types';
 import styles from './FishInfoBaitsRating.module.css';
 
 export const FishInfoBaitsRating = (props: FishInfoBaitsRatingProps) => {
   const { size = 1, value } = props;
 
-  const ranking = useMemo<number>(() => {
-    if (process.env.NODE_ENV === 'development') {
-      // return value;
-    }
-
-    switch (value) {
-      case 1:
-        return 1;
-      case 2:
-      case 3:
-        return 2;
-      default:
-        return 3;
-    }
-  }, [value]);
-
   return (
     <div className={styles.FishInfoBaitsRating} style={{ fontSize: `${size}em` }}>
-      {Array.from(Array(ranking).keys()).map(index => (
+      {Array.from(Array(value).keys()).map(index => (
         <div
           className={clsx(styles.FishInfoBaitsRatingItem, {
-            [styles.FishInfoBaitsRatingBronze]: ranking === 1,
-            [styles.FishInfoBaitsRatingSilver]: ranking === 2,
-            [styles.FishInfoBaitsRatingGold]: ranking === 3,
-            [styles.FishInfoBaitsRatingDiamond]: ranking === 4,
-            [styles.FishInfoBaitsRatingLegendary]: ranking === 5,
+            [styles.FishInfoBaitsRatingBronze]: value === 1,
+            [styles.FishInfoBaitsRatingSilver]: value === 2,
+            [styles.FishInfoBaitsRatingGold]: value === 3,
           })}
           key={index}
         >
