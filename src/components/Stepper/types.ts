7c43350cac37c6cafd43372742f3fb38b7fb489f
Codeaid <codeaid@users.noreply.github.com>
2024-05-27 21:38:49 +0100

    Updated Stepper to enable rendering ReactNode labels

--- src/components/Stepper/types.ts
+++ src/components/Stepper/types.ts
@@ -1,6 +1,8 @@
+import type { ReactNode } from 'react';
+
 export interface StepperProps<TValue extends string | number> {
   className?: string;
-  labels: Record<TValue, string>;
+  labels: Record<TValue, ReactNode>;
   selected: TValue;
   values: TValue[];
   onChange: (value: TValue) => void;
